0d8d13434795d073731d21003df28c6f9c0e7ecd
luk3Sky <ganindudananja@gmail.com>
2018-11-04 12:08:13 +0530

    Add body-parser

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const router = require('./routes/api');
+const bodyParser = require('body-parser');
 
 // Set up an express app
 const app = express();
 
-app.use('/api',router);
+// Add middleware 
+app.use(bodyParser.json());
+
+app.use('/api',require('./routes/api'));
 
 app.listen(process.env.port || 4000, function () {
     console.log('Now listening for requests!');
--- routes/api.js
+++ routes/api.js
@@ -3,25 +3,30 @@ const router = express.Router();
 
 // get a list of users from  the db
 router.get('/users', function (req, res) {
-    console.log(req);
+    console.log(req.body);
     res.send({type: 'GET'});
 });
 
 // add a new user to the db
 router.post('/users', function (req, res) {
-    console.log(req);
-    res.send({type: 'POST'});
+    console.log(req.body);    
+    res.send({
+            type: 'POST',
+            name: req.body.name,
+            age: req.body.age,
+            // skill: req.body.skill    
+    });
 });
 
 // update a new user in the db
 router.put('/users/:id', function (req, res) {
-    console.log(req);
+    console.log(req.body);
     res.send({type: 'PUT'});
 });
 
 // delete a new user in the db
 router.delete('/users/:id', function (req, res) {
-    console.log(req);
+    console.log(req.body);
     res.send({type: 'DELETE'});
 });
 
